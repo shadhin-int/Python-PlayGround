0351425a9e6c4ff622365ac50e9248e9e6bd7f9c
shadhin-int <shadhin.int@gmail.com>
2022-01-12 19:43:54 +0600

    test file update time

--- link_collector/script.py
+++ link_collector/script.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import requests
 import re
@@ -11,6 +12,7 @@ def check_url_exist(url):
                 return True
         return False
 
+
 def add_to_txt_file(url, mode):
     with open('links.txt', mode=mode) as f:
         is_url_exist = check_url_exist(url=url)
@@ -22,22 +24,24 @@ def add_to_txt_file(url, mode):
             print("Not found any new data ...!")
 
 
-
 def main():
-    for url in sys.argv[1:]:
-        # response = requests.get('https://raw.githubusercontent.com/shadhin-int/Python-Script/main/links.txt')
-        response = requests.get(url=url)
+    # for url in sys.argv[1:]:
+    response = requests.get(
+        'https://raw.githubusercontent.com/shadhin-int/Python-Script/main/links.txt')
+    # response = requests.get(url=url)
 
-        if response.status_code in [200,201]:
-            for data in response.iter_lines():
-                get_url = re.search("(?P<url>https?://[^\s]+)", str(data)).group("url")
-                is_file_exit = exists('links.txt')
-                if is_file_exit is True:
-                    add_to_txt_file(url=get_url, mode='a+')
-                else:
-                    print("File not found.... create file and add your data....")
-                    add_to_txt_file(url=get_url, mode='w+')
-
+    if response.status_code in [200, 201]:
+        for data in response.iter_lines():
+            get_url = re.search(
+                "(?P<url>https?://[^\s]+)", str(data)).group("url")
+            is_file_exit = exists('links.txt')
+            is_file_update = os.path.getmtime('links.txt')
+            print(is_file_update, "<---------------------------------")
+            if is_file_exit is True:
+                add_to_txt_file(url=get_url, mode='a+')
+            else:
+                print("File not found.... create file and add your data....")
+                add_to_txt_file(url=get_url, mode='w+')
 
 
 if __name__ == "__main__":
---
+++ link_collector/test.py
@@ -0,0 +1,21 @@
+import os
+
+# Folder Path
+path = '/media/dfs/New Volume/Github/Python-Script/link_collector/File'
+
+# Change the directory
+os.chdir(path)
+
+
+
+def read_text_file(file_path):
+	with open(file_path, 'r') as f:
+		print(f.read())
+
+
+for file in os.listdir():
+	if file.endswith(".txt"):
+		file_path = f"{path}/{file}"
+		is_file_update = os.path.getmtime(file)
+		print(is_file_update, "<-----------------")
+		read_text_file(file_path)
