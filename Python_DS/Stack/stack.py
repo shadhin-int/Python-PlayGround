634b653d84e114189063f0a45b9b4cac843839bd
Shahinur_Rahman <shadhin.int@gmail.com>
2022-01-26 12:52:22 +0600

    check if the stack is empty and peek the top value of stack

--- Python_DS/Stack/stack.py
+++ Python_DS/Stack/stack.py
@@ -11,6 +11,13 @@ class Stack():
     def pop(self):
         return self.items.pop()
     
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
     def get_stack(self):
         return self.items
 
