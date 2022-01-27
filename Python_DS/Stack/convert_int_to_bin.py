7e0c8448a8753c620ccfa52f28e457cf91d6fbe3
Shahinur Rahman <shadhin.int@gmail.com>
2022-01-27 22:50:21 +0600

    implement stack for convert decimal value to binary value

---
+++ Python_DS/Stack/convert_int_to_bin.py
@@ -0,0 +1,15 @@
+from stack import Stack
+
+stack = Stack()
+
+
+def convert_int_to_bin(dec_number):
+    bin_value = ""
+    while dec_number > 0:
+        rem = dec_number % 2
+        stack.push(rem)
+        dec_number = dec_number//2
+
+    while not stack.is_empty():
+        bin_value += str(stack.pop())
+    return bin_value
