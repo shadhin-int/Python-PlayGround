878b6253f0e7de8f261bd0fd91018b198795ae7e
Shahinur_Rahman <shadhin.int@gmail.com>
2022-01-27 16:34:15 +0600

    implement the parenthesis valid or not

---
+++ Python_DS/Stack/is_paren_balanced.py
@@ -0,0 +1,31 @@
+from Stack.stack import Stack
+
+
+def is_match(p1,p2):
+    pass
+
+
+def is_paren_balanced(paren_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "([{":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+                break
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+                    break
+        index += 1
+
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
