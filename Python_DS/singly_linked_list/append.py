3b6b4eadfa02242f628ec78f233e3ce8320966d4
Shahinur_Rahman <shadhin.int@gmail.com>
2022-01-30 14:07:44 +0600

    add data in first

--- Python_DS/singly_linked_list/append.py
+++ Python_DS/singly_linked_list/append.py
@@ -22,12 +22,20 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+    
+    def prepend(self,data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
 
 llist = LinkedList()
 llist.append("A")
 llist.append("B")
 llist.append("C")
 llist.append("D")
+
+llist.prepend("E")
 
 
 llist.print_list() 
